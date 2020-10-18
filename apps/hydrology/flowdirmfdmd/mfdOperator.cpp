41717b95943acc562d6d425540779dac9754e80e
crazyzlj <crazyzlj@gmail.com>
2020-10-18 22:34:33 +0800

    Code review

--- apps/hydrology/flowdirmfdmd/mfdOperator.cpp
+++ apps/hydrology/flowdirmfdmd/mfdOperator.cpp
@@ -5,110 +5,122 @@
 void MFDOperator::
 demLayer(RasterLayer<double> &layerD) 
 {
-  _pDEMLayer = &layerD;
-  _pDEMNbrhood = layerD.nbrhood();
-  _cellSize = _pDEMLayer->_pMetaData->cellSize;
-  _noData = _pDEMLayer->_pMetaData->noData;
-  Configure(_pDEMLayer, false);
+    _pDEMLayer = &layerD;
+    _pDEMNbrhood = layerD.nbrhood();
+    _cellSize = _pDEMLayer->_pMetaData->cellSize;
+    _noData = _pDEMLayer->_pMetaData->noData;
+    Configure(_pDEMLayer, false);
 }
 
 void MFDOperator::weightLayers(vector<RasterLayer<double> *> &layers) 
 {
-  for( int i=0; i<8; ++i ){
-	  _weightLayers.push_back(layers[i]);
-	  Configure(_weightLayers[i],false);
-  }
+    for( int i=0; i<8; ++i ){
+        _weightLayers.push_back(layers[i]);
+        Configure(_weightLayers[i],false);
+    }
 }
 
 bool MFDOperator::isTermination()
 {
-	num--;
-	if(num > 0)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+    num--;
+    return num > 0;
+    // The if-else can be simplified by `return num > 0` --LJ
+    //if(num > 0)
+    //{
+    //	return true;
+    //}
+    //else
+    //{
+    //	return false;
+    //}
 }
 
 
 bool MFDOperator::Operator(const CellCoord &coord,bool operFlag)
 {	
-	CellSpace<double> &demL = *(_pDEMLayer->cellSpace());
-	vector<CellSpace<double>* > weightLs;
-	for( int i=0; i<8; ++i ){
-		weightLs.push_back(_weightLayers[i]->cellSpace());
-	}
-	Neighborhood<double>& nbrhoodD = *(_pDEMNbrhood);
+    CellSpace<double> &demL = *(_pDEMLayer->cellSpace());
+    vector<CellSpace<double>* > weightLs(8);
+    for( int i=0; i<8; ++i ){
+        weightLs[i] = _weightLayers[i]->cellSpace();
+    }
+    Neighborhood<double>& nbrhoodD = *(_pDEMNbrhood);
 	
-	int iRow = coord.iRow();
-	int iCol = coord.iCol();
-	int iNeighborCells = ((int)sqrt((double)nbrhoodD.size())) / 2;
+    int iRow = coord.iRow();
+    int iCol = coord.iCol();
+    // TODO: Considering should floor() or ceil() be necessary? I think this should be one function of `Neighborhood` class. --LJ
+    //       Or `int GetRowNum() { return sqrt((double)(_pDEMNbrhood->size() - 1) / 2); }`? --line 29 in LEOperator.h
+    int iNeighborCells = ((int)sqrt((double)nbrhoodD.size())) / 2;
 	
+    // TODO: Is it possible to add a function to checkout NODATA for `CellSpace` like demL.isNodata(iRow, iCol)? --LJ
+    if (fabs(demL[iRow][iCol] - _noData) <= Eps) {
+        for (int i = 0; i < 8; ++i) {
+            (*weightLs[i])[iRow][iCol] = _noData;
+        }
+        return true;
+    }
+    //if(fabs(demL[iRow][iCol] - _noData) > Eps){
+        for( int i=0; i<8; ++i ){
+            (*weightLs[i])[iRow][iCol] = 0.0;
+        }
+        //cout<<rowNum<<colNum<<endl;
+        //double d[9];//邻域栅格高程
+        int dir = 0;             //邻域栅格编号
+        double maxDiffVal = 0.0; //最大坡降正切值
+        double pe = 0.0;         //公式中的坡度指数p
+        double l[9];             //weighted factor of contour line; parameter in equation
+        double diffVal[9];
 
-	if(fabs(demL[iRow][iCol] - _noData) > Eps){
-		for( int i=0; i<8; ++i ){
-			(*weightLs[i])[iRow][iCol] = 0.0;
-		}
-		//cout<<rowNum<<colNum<<endl;
-		//double d[9];//邻域栅格高程
-		int dir = 0;//邻域栅格编号
-		double maxDiffVal = 0.0;//最大坡降正切值
-		double pe = 0.0;//公式中的坡度指数p
-		double l[9];//weighted factor of contour line; parameter in equation
-		double diffVal[9];
+        // TODO: Some commonly used constants should be predefined in a high precision, e.g., SQ2 = 1.4142135623730951
+        // Refer to the origin implementation in SimDTA/src/modMFD.bas, start from line 457. --LJ
+        for(int tRow=iRow-iNeighborCells; tRow<=iRow+iNeighborCells; tRow++){
+            for(int tCol=iCol-iNeighborCells; tCol<=iCol+iNeighborCells; tCol++){
+                if(fabs(demL[tRow][tCol] - _noData) <= Eps){
+                    //if(dir<4)
+                    //	*(weightLs[dir])[iRow][iCol] = 0;
+                    //else
+                    //	*(weightLs[dir-1])[iRow][iCol] = 0;
+                    diffVal[dir] = 0.0;
+                    l[dir] = 0.0;
+                }else{
+                    if((demL[iRow][iCol] <= demL[tRow][tCol])){
+                        diffVal[dir] = 0.0;
+                        l[dir] = 0.0;
+                    }else{
+                        if(dir==0||dir==2||dir==6||dir==8){
+                            diffVal[dir] = (demL[iRow][iCol]-demL[tRow][tCol])/(1.414*_cellSize);
+                            l[dir] = 0.354*_cellSize; //? -- sqrt(2)/4  Should be precisely predefined! --LJ
+                        }else{
+                            diffVal[dir] = (demL[iRow][iCol]-demL[tRow][tCol])/_cellSize;
+                            l[dir] = 0.5*_cellSize;
+                        }
+                        if (diffVal[dir] > maxDiffVal)	
+                            maxDiffVal = diffVal[dir];
+                    }
+                }
+                dir++;
+            }
+        }
+        if(maxDiffVal > 1)
+            maxDiffVal = 1;
+        pe = (1-slpExp)*maxDiffVal + slpExp;
+        double sum = 0.0;
+        for(int i=0;i<9;i++){
+            sum += pow(diffVal[i],pe)*l[i];
+        }
+        if(sum > 0){
+            for(int i=0;i<4;i++){
+                (*weightLs[i])[iRow][iCol] = (pow(diffVal[i],pe)*l[i])/sum ;
+            }
+            for(int i=5;i<9;i++){
+                (*weightLs[i-1])[iRow][iCol] = (pow(diffVal[i],pe)*l[i])/sum ;
+            }
+        }
+    //}
+    //else{ // This else should be excluded at the front, which can save a indent of the above code. --LJ
+    //    for( int i=0; i<8; ++i ){
+    //        (*weightLs[i])[iRow][iCol] = _noData;
+    //    }
+    //}
 
-		for(int tRow=iRow-iNeighborCells; tRow<=iRow+iNeighborCells; tRow++){
-			for(int tCol=iCol-iNeighborCells; tCol<=iCol+iNeighborCells; tCol++){
-				if(fabs(demL[tRow][tCol] - _noData) <= Eps){
-					//if(dir<4)
-					//	*(weightLs[dir])[iRow][iCol] = 0;
-					//else
-					//	*(weightLs[dir-1])[iRow][iCol] = 0;
-					diffVal[dir] = 0.0;
-					l[dir] = 0.0;
-				}else{
-					if((demL[iRow][iCol] <= demL[tRow][tCol])){
-						diffVal[dir] = 0.0;
-						l[dir] = 0.0;
-					}else{
-						if(dir==0||dir==2||dir==6||dir==8){
-							diffVal[dir] = (demL[iRow][iCol]-demL[tRow][tCol])/(1.414*_cellSize);
-							l[dir] = 0.354*_cellSize;//?
-						}else{
-							diffVal[dir] = (demL[iRow][iCol]-demL[tRow][tCol])/_cellSize;
-							l[dir] = 0.5*_cellSize;
-						}
-						if (diffVal[dir] > maxDiffVal)	
-							maxDiffVal = diffVal[dir];
-					}
-				}
-				dir++;
-			}
-		}
-		if(maxDiffVal > 1)
-			maxDiffVal = 1;
-		pe = (1-slpExp)*maxDiffVal + slpExp;
-		double sum = 0.0;
-		for(int i=0;i<9;i++){
-			sum += pow(diffVal[i],pe)*l[i];
-		}
-		if(sum > 0){
-			for(int i=0;i<4;i++){
-				(*weightLs[i])[iRow][iCol] = (pow(diffVal[i],pe)*l[i])/sum ;
-			}
-			for(int i=5;i<9;i++){
-				(*weightLs[i-1])[iRow][iCol] = (pow(diffVal[i],pe)*l[i])/sum ;
-			}
-		}
-	}else{
-		for( int i=0; i<8; ++i ){
-			(*weightLs[i])[iRow][iCol] = _noData;
-		}
-	}
-
-	return true;
+    return true;
 }
-
--- apps/hydrology/flowdirmfdmd/pMFDmd.cpp
+++ apps/hydrology/flowdirmfdmd/pMFDmd.cpp
@@ -2,7 +2,8 @@
 * pMFDmd.cpp
 *
 * Project: GPRO_MFDmd
-* Purpose: Calculation of flow direction from DEM according to MFDmd (Qin2007); Demonstration program for GPRO. 
+* Purpose: Calculation of flow direction from DEM according to MFDmd (Qin et al., IJGIS, 2007).
+*          A demonstration program for GPRO.
 *
 * Author:  Ai Beibei
 * E-mail:  aibb@lreis.ac.cn
@@ -10,167 +11,173 @@
 * Copyright (c) 2017. Ai Beibei
 * 
 ****************************************************************************/
-
-
 #include <iostream>
 #include <string>
-#include <fstream>
-#include <time.h>
-#include <sstream>
-#include <omp.h>
+#include <ctime>
 #include "mpi.h"
+
 #include "neighborhood.h"
-#include "cellSpace.h"
 #include "basicTypes.h"
 #include "rasterLayer.h"
 #include "application.h"
 #include "mfdOperator.h"
-#include "communication.h"
 
-
-using namespace std;
+// using namespace std; // DO NOT use this statement in case of unexpected errors such as redefinitions of macros --LJ
 using namespace GPRO;
 
 void Usage(const string& error_msg = "") {
-
     if (!error_msg.empty()) {
         cout << "FAILURE: " << error_msg << endl << endl;
     }
-    cout << " Usage: flowdirmfdmd -elev <elevation grid file> -nbr <neighbor definition file> -out <output flow direction file> -exp <slope exponent>" << endl;
-    cout << " the default slope exponent is 1.1. For more details please see Qin 2007 or SimDTA manual" << endl<<endl;
-    cout << " Or use the Simple Usage: flow <elevation grid file> <neighbor definition file> <output flow direction file> <slope exponent>" << endl << endl;
+    cout <<
+            " Usage: flowdirmfdmd -elev <elevation grid file> -nbr <neighbor definition file> -out <output flow direction file> -exp <slope exponent>"
+            << endl;
+    cout << " the default slope exponent is 1.1. For more details please see Qin 2007 or SimDTA manual" << endl << endl;
+    cout <<
+            " Or use the Simple Usage: flow <elevation grid file> <neighbor definition file> <output flow direction file> <slope exponent>"
+            << endl << endl;
     cout << "Example.1. flowdirmfdmd -elev /path/to/elev.tif -nbr /path/to/moore.nbr -out /path/to/mfdmd.tif" << endl;
     cout << "Example.4. flowdirmfdmd /path/to/elev.tif /path/to/moore.nbr /path/to/mfdmd.tif" << endl;
 
     exit(1);
 }
 
-int main(int argc, char *argv[]) 
-{
-
-	/*!
-	 * Parse input arguments.
-	 * DO NOT start the application unless the required inputs are provided!
-	 */
-	if (argc < 5) {
+int main(int argc, char* argv[]) {
+    /*!
+     * Parse input arguments.
+     * DO NOT start the application unless the required inputs are provided!
+     */
+    if (argc < 5) {
         Usage("Too few arguments to run this program.");
-	}
-	// Input arguments
-	char* inputfilename = nullptr;
-	char* neighborfile = nullptr;
-	char* outputfilename = nullptr;
-	double slpExp;
+    }
+    // Input arguments
+    char* inputfilename = nullptr;
+    char* neighborfile = nullptr;
+    char* outputfilename = nullptr;
+    double slpExp = -1.; // Should have a default value in case of no input --LJ
+    char* slpExpStrErr = nullptr;
 
     int i = 1;
     bool simpleusage = true;
-	while (argc > i) {
-		if (strcmp(argv[i], "-elev") == 0) {
+    while (argc > i) {
+        if (strcmp(argv[i], "-elev") == 0) {
             simpleusage = false;
             i++;
-			if (argc > i) {
+            if (argc > i) {
                 inputfilename = argv[i];
                 i++;
             } else {
-	            Usage("No argument followed '-elev'!");
+                Usage("No argument followed '-elev'!");
             }
-		} else if (strcmp(argv[i], "-nbr") == 0) {
+        } else if (strcmp(argv[i], "-nbr") == 0) {
             simpleusage = false;
             i++;
-			if (argc > i) {
+            if (argc > i) {
                 neighborfile = argv[i];
                 i++;
-			} else {
-				Usage("No argument followed '-nbr'!");
-			}
+            } else {
+                Usage("No argument followed '-nbr'!");
+            }
         } else if (strcmp(argv[i], "-out") == 0) {
             simpleusage = false;
             i++;
-			if (argc > i) {
+            if (argc > i) {
                 outputfilename = argv[i];
                 i++;
-			} else {
+            } else {
                 Usage("No argument followed '-out'!");
-			}
+            }
         } else if (strcmp(argv[i], "-exp") == 0) {
             simpleusage = false;
             i++;
-			if (argc > i) {
-                slpExp = atof(argv[i]);
+            if (argc > i) {
+                // atof not report error, consider using strtod instead. --LJ
+                // slpExp = atof(argv[i]);
+                slpExp = strtod(argv[4], &slpExpStrErr);
                 i++;
-			} else {
+            } else {
                 Usage("No argument followed '-exp'!");
-			}
-        } else { // Simple Usage
+            }
+        } else {
+            // Simple Usage
             if (!simpleusage) Usage("DO NOT mix the Full and Simple usages!");
             inputfilename = argv[1];
             neighborfile = argv[2];
             outputfilename = argv[3];
-			if(argc>4){
-				slpExp = atof(argv[4]);
-			}
-			break;
+            if (argc > 4) {
+                // slpExp = atof(argv[4]);
+                slpExp = strtod(argv[4], &slpExpStrErr);
+            }
+            break;
         }
-	}
-	if( !( slpExp>=0 || slpExp<=1 ) ){
-		cerr<<"please input slope exponent between 0-1.";
-		return 0;
-	}
+    }
+    if (!(slpExp >= 0 || slpExp <= 1)) {
+        cerr << "please input slope exponent between 0-1.";
+        return 0;
+    }
 
-	Application::START(MPI_Type, argc, argv); //init
+    Application::START(MPI_Type, argc, argv); // init MPI environment
 
-	RasterLayer<double> demLayer("demLayer"); //创建图层
-	demLayer.readNeighborhood(neighborfile);  //读取分析窗口文件
-	demLayer.readFile(inputfilename);  //读取栅格数据
+    RasterLayer<double> demLayer("demLayer"); // create raster layer (DEM)
+    demLayer.readNeighborhood(neighborfile);  // read analysing neighbor window for raster layer
+    demLayer.readFile(inputfilename);         // read data of layer (DEM)
 
-	//creat output filenamme for weight matrix layers
-	//string filepath = neighborfile;
-	//size_t idx = filepath.find_last_of('/');
-	//if( idx >= filepath.size() )
-	//	idx = filepath.find_last_of('\\');
-	//filepath.assign(filepath,0,idx+1);
-	//const char* cFilepath = filepath.c_str();
-	vector<RasterLayer<double> *> weightLayers;
-	char** pOutputfilenames = new char*[8];
-	for( int i=0; i<8; ++i ){
-		char* tmp = new char [200];
-		//sprintf(tmp, "%sweightLayer%d.tif", cFilepath,i);
-		if( i<4 )
-			sprintf(tmp, "%sweightLayer%d.tif", outputfilename,i);
-		else
-			sprintf(tmp, "%sweightLayer%d.tif", outputfilename,i+1);
-		pOutputfilenames[i] = tmp;
-		//RasterLayer<double> outLayer(pOutputfilenames[i]);
-		RasterLayer<double> *outLayer = new RasterLayer<double> (pOutputfilenames[i]);
-		weightLayers.push_back(outLayer);
-		weightLayers[i]->copyLayerInfo(demLayer);
-	}
-	
-	double starttime;
-	double endtime;
-	MPI_Barrier(MPI_COMM_WORLD);
-	starttime = MPI_Wtime();
+    // creat output filenamme for weight matrix layers
+    vector<RasterLayer<double>*> weightLayers;
+    int flowdir_count = 8;
+    char** pOutputfilenames = new char*[flowdir_count];
+    for (int i = 0; i < flowdir_count; i++) {
+        char* tmp = new char[200];
+        // TODO: This is wrong.
+        if (i < 4)
+            sprintf(tmp, "%sweightLayer%d.tif", outputfilename, i);
+        else
+            sprintf(tmp, "%sweightLayer%d.tif", outputfilename, i + 1);
+        pOutputfilenames[i] = tmp;
+        RasterLayer<double>* outLayer = new RasterLayer<double>(pOutputfilenames[i]);
+        weightLayers.push_back(outLayer);
+        weightLayers[i]->copyLayerInfo(demLayer);
+    }
 
-	MFDOperator SFlOper(slpExp);	
-	SFlOper.demLayer(demLayer);
-	SFlOper.weightLayers(weightLayers);
-	SFlOper.Run();
+    MPI_Barrier(MPI_COMM_WORLD);
+    double starttime = MPI_Wtime();
 
-	MPI_Barrier(MPI_COMM_WORLD);
-	endtime = MPI_Wtime();
-	int myrank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-	if( myrank==0 )
-	cout<<"run time is "<<endtime-starttime<<endl;
+    MFDOperator SFlOper(slpExp);
+    SFlOper.demLayer(demLayer);
+    SFlOper.weightLayers(weightLayers);
+    SFlOper.Run();
 
-	for( int i=0; i<8; ++i ){
-		weightLayers[i]->writeFile(pOutputfilenames[i]);
-	}
-	
-	MPI_Barrier(MPI_COMM_WORLD);
-	//pOutputfilenames释放内存
-	delete []pOutputfilenames;
-	//weightLayers释放内存
+    for (int i = 0; i < flowdir_count; i++) {
+        weightLayers[i]->writeFile(pOutputfilenames[i]);
+    }
 
-	Application::END();
-	return 0;
+    MPI_Barrier(MPI_COMM_WORLD);
+    double endtime = MPI_Wtime();
+
+    int myrank = GetRank();
+    // Should print the maximum time-consuming of all ranks. Data IO time should also be included. So, counting IO and computing separately. --LJ
+    //if (myrank == 0)
+    //    cout << "run time is " << endtime - starttime << endl;
+    double t_all_rank = endtime - starttime;
+    
+    double t_all;  ///< Maximum time-consuming of parallel tasks in all ranks
+    
+    MPI_Reduce(&t_all_rank, &t_all, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    if (myrank == 0) {
+        cout << "run time is " << t_all << endl;
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // For 2d-arrary, a for-loop should be used to release memory. --LJ
+    // delete []pOutputfilenames;
+    for (int i = 0; i < flowdir_count; i++) {
+        delete[] pOutputfilenames[i];
+        pOutputfilenames[i] = nullptr;
+    }
+    delete[] pOutputfilenames;
+    pOutputfilenames = nullptr;
+
+    Application::END();
+    return 0;
 }
